bb26f9cd364feb65dc022cd17df9c2c26c483a11
igl-myrick <igl.myrick@gmail.com>
2025-06-24 13:55:35 -0700

    add ApiHelper class

---
+++ CretaceousClient/Models/ApiHelper.cs
@@ -0,0 +1,15 @@
+using RestSharp;
+
+namespace CretaceousClient.Models
+{
+  public class ApiHelper
+  {
+    public static async Task<string> GetAll()
+    {
+      RestClient client = new RestClient("http://localhost:5000/");
+      RestRequest request = new RestRequest($"api/animals", Method.Get);
+      RestResponse response = await client.GetAsync(request);
+      return response.Content;
+    }
+  }
+}
